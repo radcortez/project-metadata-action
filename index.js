87c52924c3e28dad1be9bed8f6cdf0de8e93d0a2
Michael Edgar <medgar@redhat.com>
2021-04-21 16:03:53 -0400

    Add option for reading locally-checked out file
    Signed-off-by: Michael Edgar <medgar@redhat.com>

--- index.js
+++ index.js
@@ -1,38 +1,52 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const yaml = require('js-yaml');
+const { promises: fs } = require('fs');
 
 try {
     const metadataFilePath = core.getInput('metadata-file-path');
-    console.log(`Reading file from ${metadataFilePath}`);
+    const localFile = core.getInput('local-file');
 
-    const octokit = new github.GitHub(core.getInput('github-token'));
-
-    octokit.pulls.listFiles({
-        owner: github.context.repo.owner,
-        repo: github.context.repo.repo,
-        pull_number: github.context.payload.pull_request.number
-    }).then(({data}) => {
-
-        let metadataFile = data.find(function (file) {
-            return file.filename === metadataFilePath;
-        });
-
-        if (metadataFile == null) {
-            console.log(`Could not find file to read project metadata`);
-            return;
-        }
-
-        octokit.request("GET " + metadataFile.raw_url)
-            .then(({data}) => {
+    if (localFile === 'true') {
+        console.log(`Reading workspace file ${metadataFilePath}`);
+        fs.readFile(metadataFilePath, 'utf8')
+            .then(data => {
+                console.debug(`File found ${metadataFilePath}`);
                 let metadata = yaml.safeLoad(data);
                 setOutputs(metadata);
+            })
+            .catch(err => {
+                core.setFailed(`Could not read project metadata file: ${err}`);
+            });
+    } else {
+        console.log(`Reading file from ${metadataFilePath}`);
+        const octokit = new github.GitHub(core.getInput('github-token'));
+
+        octokit.pulls.listFiles({
+            owner: github.context.repo.owner,
+            repo: github.context.repo.repo,
+            pull_number: github.context.payload.pull_request.number
+        }).then(({data}) => {
+
+            let metadataFile = data.find(function (file) {
+                return file.filename === metadataFilePath;
             });
 
-    }).catch((error) => {
-        console.debug(error);
-        core.setFailed('Unknown Error!')
-    })
+            if (metadataFile == null) {
+                console.log(`Could not find file to read project metadata`);
+                return;
+            }
+
+            octokit.request("GET " + metadataFile.raw_url)
+                .then(({data}) => {
+                    let metadata = yaml.safeLoad(data);
+                    setOutputs(metadata);
+                });
+        }).catch((error) => {
+            console.debug(error);
+            core.setFailed('Unknown Error!')
+        })
+    }
 
 } catch (error) {
     core.setFailed(error.message);
